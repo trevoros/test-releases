dbce78395fd6e5eead1375ca7bf0a0b575e5452a
Rosmel Alvarez <rosmelv@gmail.com>
2024-03-28 03:06:50 -0400

    added express

---
+++ index.js
@@ -0,0 +1,15 @@
+// Import the express library
+const express = require('express');
+
+// Create an instance of express
+const app = express();
+
+// Define a route handler for GET requests made to the root path ('/')
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Have the app listen on port 3000
+app.listen(3000, () => {
+  console.log('App is listening on port 3000');
+});
